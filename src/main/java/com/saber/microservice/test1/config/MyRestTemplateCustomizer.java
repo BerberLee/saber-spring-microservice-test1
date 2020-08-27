cdbc8a0a9a266203a32c0f1f10225ccfb41cd5b0
BerberLee <saberazizi66@yahoo.com>
2020-08-28 02:29:55 +0430

    remove services and add RestTemplate Config

---
+++ src/main/java/com/saber/microservice/test1/config/MyRestTemplateCustomizer.java
@@ -0,0 +1,44 @@
+package com.saber.microservice.test1.config;
+
+import io.micrometer.core.instrument.binder.httpcomponents.PoolingHttpClientConnectionManagerMetricsBinder;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class MyRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private ClientHttpRequestFactory clientHttpRequestFactory(){
+
+        PoolingHttpClientConnectionManager connectionManager= new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(150);
+        connectionManager.setDefaultMaxPerRoute(25);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(2000)
+                .setSocketTimeout(2000)
+                .build();
+
+        CloseableHttpClient httpClient= HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(clientHttpRequestFactory());
+    }
+}
